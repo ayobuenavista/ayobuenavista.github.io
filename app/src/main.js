17920b05194322f83324cb686153b95511a97ce6
Anton Buenavista <ay.buenavista@gmail.com>
2018-03-14 16:13:42 +0800

    Added Google Analytics via vue-analytics

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import VuePrism from 'vue-prism';
 import App from '@/App';
 import router from '@/router';
@@ -8,6 +9,9 @@ import router from '@/router';
 import Particles from '@/components/Particles';
 import Resume from '@/components/Resume';
 
+Vue.use(VueAnalytics, {
+  id: 'UA-115783431-1',
+});
 Vue.use(Particles, VuePrism);
 Vue.component('Particles', Particles);
 Vue.component('Resume', Resume);
